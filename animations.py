3b69585f468ca3e69942ca260f160429870e6294
Samuel Garrido <tayllard@tutanota.com>
2021-04-23 13:00:35 -0300

    Att 1

--- animations.py
+++ animations.py
@@ -1,2 +1,7 @@
 from time import sleep
-print('1', end=" - ")
+
+def animation(ani,vezes):
+    for x in range(0, vezes):
+        print(ani);sleep(1)
+
+animation("369",3)
--- game.py
+++ game.py
@@ -6,11 +6,12 @@
 #\033[34;35m
 
 #Importação das bibliotecas
+from posix import sched_param
 from time import sleep
 
 
 #Atualização de variáveis.
-import help,shop
+import help,shop,os
 
 print("Olá, é a primeira vez que vejo você aqui. Parece que o destino nos uniu.");sleep(0.5)
 nome = input("Já que estamos por aqui. Qual é o seu nome? ");sleep(1)
@@ -24,12 +25,13 @@ while True:
     if ordem == "HELP":
         help.help()
         print("Fim do comando help")
-    if ordem == "EXIT":
+    elif ordem == "EXIT":
         print("Adeus, talvez nos encontraremos novamente, ou não.")
         break
-    if ordem == "SHOP":
+    elif ordem == "SHOP":
         print("Bem-Vindo a loja o que desejas comprar?");sleep(2)
         shop.shop()
 
     else:
+        os.system("cls")
         print("Comando inválido tente novamente, você digitou -> " + ordem)
